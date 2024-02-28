1dd1b0119bc1853ed498d4ace4468df12c64d477
robmoncayonabux <roberto.moncayo@nabux.ec>
2024-02-28 13:56:32 -0500

    Its working from here

---
+++ ig_to_sheets1_PB.py
@@ -0,0 +1,55 @@
+import requests
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Configuración de Instagram API
+ACCESS_TOKEN = 'EAAFKUx7Tl00BO0jOFvZAcHIMrIdrBkUz1PjeLLO4TcdkiegANyzOrgdJHZAMnh8TcU51OAonrwU1DyLoi1hziZBqdI38ddxOtRs6uBZBOO1bImvm5LUhp0WNKbipBZCevi7n8sZBYx8ZCYDWWbFcyqdNRznQGT5mVpj1vUbQC8OxWXC7uR3rJ1BnbrOPuV1q7BhNhJrjywgo4G3UBQexRGPE10lyNGtfywXcXRdKk9b'
+INSTAGRAM_BUSINESS_ACCOUNT_ID = '17841449053633062'
+
+def get_instagram_post_insights():
+    url = f"https://graph.facebook.com/v19.0/{INSTAGRAM_BUSINESS_ACCOUNT_ID}/media?fields=id,caption,media_type,media_url,permalink,timestamp,insights.metric(impressions,reach,saved,video_views)&access_token={ACCESS_TOKEN}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error al realizar la solicitud a la API: {e}")
+        return None
+
+# Obtener datos de Instagram
+datos_instagram = get_instagram_post_insights()
+
+# Procesar con pandas
+if datos_instagram:
+    df = pd.json_normalize(datos_instagram['data'])
+
+    # Expandir los insights.data a columnas separadas
+    def expand_insights(row):
+        insights_data = row.get('insights.data', [])
+        insights_dict = {item['name']: item['values'][0]['value'] for item in insights_data}
+        return pd.Series(insights_dict)
+    
+    # Aplicar la función para expandir los insights y unirlos al DataFrame principal
+    insights_expanded = df.apply(expand_insights, axis=1)
+    df = pd.concat([df.drop(columns=['insights.data']), insights_expanded], axis=1)
+    
+    df.fillna(value="", inplace=True)
+
+    # Configuración de Google Sheets
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('data-analyst-415118-c4756ee9fde6.json', scope)
+    client = gspread.authorize(creds)
+
+    # Nombre de tu hoja de cálculo y seleccionar la primera hoja
+    sheet = client.open("HOJA").sheet1
+
+    # Preparar datos para enviar a Google Sheets
+    datos_para_google_sheets = df.values.tolist()
+    datos_para_google_sheets.insert(0, df.columns.to_list())
+
+    datos_para_google_sheets = [[str(item) for item in row] for row in datos_para_google_sheets]
+    sheet.update('A1', datos_para_google_sheets)
+    print("Datos enviados a Google Sheets con éxito.")
+else:
+    print("No se pudieron obtener los datos de Instagram.")
---
+++ ig_to_sheets2_PB.py
@@ -0,0 +1,44 @@
+import requests
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Configuración de Instagram API
+ACCESS_TOKEN = 'EAAFKUx7Tl00BO0jOFvZAcHIMrIdrBkUz1PjeLLO4TcdkiegANyzOrgdJHZAMnh8TcU51OAonrwU1DyLoi1hziZBqdI38ddxOtRs6uBZBOO1bImvm5LUhp0WNKbipBZCevi7n8sZBYx8ZCYDWWbFcyqdNRznQGT5mVpj1vUbQC8OxWXC7uR3rJ1BnbrOPuV1q7BhNhJrjywgo4G3UBQexRGPE10lyNGtfywXcXRdKk9b'
+INSTAGRAM_BUSINESS_ACCOUNT_ID = '17841449053633062'
+
+def get_extra_instagram_data():
+    # Ajusta esta solicitud para incluir datos sobre likes y comentarios
+    url = f"https://graph.facebook.com/v19.0/{INSTAGRAM_BUSINESS_ACCOUNT_ID}/media?fields=id,like_count,comments_count&access_token={ACCESS_TOKEN}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error al realizar la solicitud a la API: {e}")
+        return None
+
+# Obtener datos adicionales de Instagram
+extra_datos_instagram = get_extra_instagram_data()
+
+# Procesar con pandas
+if extra_datos_instagram:
+    df_extra = pd.json_normalize(extra_datos_instagram['data'])
+
+    # Configuración de Google Sheets
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('data-analyst-415118-c4756ee9fde6.json', scope)
+    client = gspread.authorize(creds)
+
+    # Seleccionar la hoja específica para los datos adicionales
+    sheet_name = "HOJA"
+    sheet = client.open(sheet_name).get_worksheet(1)  # Asume que '1' es el índice para sheet2
+
+    # Preparar y enviar datos adicionales a Google Sheets
+    datos_extra_para_google_sheets = df_extra.values.tolist()
+    datos_extra_para_google_sheets.insert(0, df_extra.columns.to_list())
+    datos_extra_para_google_sheets = [[str(item) for item in row] for row in datos_extra_para_google_sheets]
+    sheet.update('A1', datos_extra_para_google_sheets)
+    print("Datos adicionales enviados a Google Sheets con éxito.")
+else:
+    print("No se pudieron obtener los datos adicionales de Instagram.")
